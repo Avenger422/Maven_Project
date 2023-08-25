7da5ee66e747e5f1f9d7c58f1a09c5c0b9f10224
Avenger422 <134751260+Avenger422@users.noreply.github.com>
2023-08-26 05:27:58 +0530

    Add files via upload

---
+++ src/test/java/MyProject/AppTest.java
@@ -0,0 +1,31 @@
+package MyProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void hello()
+    {
+    	System.out.println("Hello world");
+    }
+    @Test
+    public void helloWorld()
+    {
+    	System.out.println("Hello dev2");
+    }
+    
+}
